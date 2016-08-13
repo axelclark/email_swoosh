1b09310eb5cd3a28e82d10c796a279622a0c3c8f
Axel Clark <axelclark2@yahoo.com>
2016-08-13 18:36:54 -0400

    Update SendGrid config

--- config/config.exs
+++ config/config.exs
@@ -24,7 +24,7 @@ config :logger, :console,
 
 config :email_swoosh, EmailSwoosh.Mailer,
   adapter: Swoosh.Adapters.Sendgrid,
-  api_key: {:system, "SENDGRID_API_KEY"}
+  api_key: System.get_env("SENDGRID_API_KEY")
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
