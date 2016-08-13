b93c636b246b19727813c11a6ab837b5bbf3a646
Axel Clark <axelclark2@yahoo.com>
2016-08-13 15:37:54 -0400

    Update config for Heroku

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,9 @@ defmodule EmailSwoosh.UserSocket do
   # channel "room:*", EmailSwoosh.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
+
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
