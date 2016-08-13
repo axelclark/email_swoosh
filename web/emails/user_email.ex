8c363d704a5774b6394daef875c831cd580f3f04
Axel Clark <axelclark2@yahoo.com>
2016-08-13 18:51:24 -0400

    Update welcome email

--- web/emails/user_email.ex
+++ web/emails/user_email.ex
@@ -6,7 +6,7 @@ defmodule EmailSwoosh.UserEmail do
     |> to({"Axel", "axelclark2@yahoo.com"})
     |> from({"Dr B Banner", "hulk@smash.com"})
     |> subject("Hello, Avengers!")
-    |> html_body("<h1>Hello Axel</h1>")
+    |> html_body("<h1>Hello Axel!</h1>")
     |> text_body("Hello Axel")
   end
 end
